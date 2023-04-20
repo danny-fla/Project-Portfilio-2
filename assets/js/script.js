27f0a31be84d95f63c62171b06230fec58280cad
danny-fla <dannyfla@live.ie>
2023-04-20 16:57:23 +0000

    add questions to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,62 @@
-console.log("howshe now")
+const questions = [
+{
+    1: "Madrid",
+    2: "Valencia",
+    3: "Barcelona",
+    4: "Sevilla",
+    answer: "Barcelona",
+}, {
+    1: "Dublin",
+    2: "Liverpool",
+    3: "Belfast",
+    4: "Cardiff",
+    answer: "Dublin",
+}, {
+    1: "Venice",
+    2: "Rome",
+    3: "Milan",
+    4: "Turin",
+    answer: "Rome",
+}, {
+    1: "Thailand",
+    2: "Cambodia",
+    3: "Malaysia",
+    4: "Vietnam",
+    answer: "Vietnam",
+}, {
+    1: "San Francisco",
+    2: "San Antonio",
+    3: "Seattle",
+    4: "New York",
+    answer: "San Francisco",
+}, {
+    1: "Bolivia",
+    2: "Brazil",
+    3: "Argentina",
+    4: "Mexico",
+    answer: "Brazil",
+}, {
+    1: "Spain",
+    2: "Germany",
+    3: "Italy",
+    4: "France",
+    answer: "France",
+}, {
+    1: "Spain",
+    2: "France",
+    3: "Germany",
+    4: "Italy",
+    answer: "Spain",
+}, {
+    1: "South Korea",
+    2: "China",
+    3: "Japan (Ushiku Daibutsu)",
+    4: "India",
+    answer: "Japan",
+}, {
+    1: "London",
+    2: "Birmingham",
+    3: "Manchester",
+    4: "Newcastle",
+    answer: "London",
+}]
