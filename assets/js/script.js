9297d33878433368c4c174d7909f5144ae221888
danny-fla <dannyfla@live.ie>
2023-05-04 20:31:37 +0000

    add onclick to next/retry btns.add functions to: calculate the correct user answer and indicate if they are correct/incorrect, generate a new question, restart quiz and timer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,18 +91,19 @@ let questionSelector = [];
 let userAnswers = [];
 let questionsAnswered = 0;
 const MAX_QUESTIONS = 3;
-let currentQuestion = 0;
+let currentQuestion = {};
 let questionCounter = document.querySelector('#question-counter');
-let counter = 0;
+let counter;
 let userScore = document.querySelector('#user-score')
 let gameProgress
 
 const secondsDisplay = document.querySelector('#seconds');
-let time = 5;
+let time;
 let timerInterval;
 
 let optionBtns = document.querySelectorAll('.option-btn')
 let quizImage = document.querySelector('#landmark');
+
 // buttons
 
 let btns = document.querySelectorAll('.button')
@@ -145,17 +146,17 @@ function beginQuiz() {
 
     correctUserAnswers = 0;
     availableQuestions = [...quizQuestions];
-    secondsDisplay.textContent = 15;
+    secondsDisplay.textContent;
     userScore.textContent = 0;
     generateNewQuestion();
-    // resetTimer();
-    // startTimer();
+    resetTimer();
+    startTimer();
 }
 
 // attaches event listener to each option button and envokes check answer function when clicked
 
 optionBtns.forEach(function (button) {
-    button.addEventListener('click', checkAnswer);
+    button.addEventListener('click', calculateAnswer);
 });
 
 function generateNewQuestion() {
@@ -167,23 +168,9 @@ function generateNewQuestion() {
         console.log('gameover');
         return;
     }
-
-//     availableQuestions.splice(currentQuestion, 1);
-
-//     [currentQuestion] = availableQuestions.sort(() => Math.random() - 0.5);
-//     const {imageSrc, options} = availableQuestions[currentQuestion];
-    
-//     quizImage.src = imageSrc;
-    
-//    optionBtns.forEach((btn,i) => {
-//     btn.textContent = options[i].text;
-//     btn.dataset.correct = options[i].isCorrect;
-//    });
-// }
     let shuffleQuestions = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = shuffleQuestions;
     let question = availableQuestions[currentQuestion];
-
     quizImage.src = question.imageSrc;
 
     for (let i = 0; i < optionBtns.length; i++) {
@@ -194,4 +181,88 @@ function generateNewQuestion() {
         optionBtns[i].textContent = question.options[i].text;
         optionBtns[i].dataset.correct = question.options[i].isCorrect;
     }
+    
 }
+
+function calculateAnswer(event) {
+    let selectedOption = event.target;
+    let isCorrect = selectedOption.dataset.correct === "true";
+
+    if (isCorrect) {
+        selectedOption.classList.add("correct");
+        userScore.textContent = ++score;
+    } else {
+        selectedOption.classList.add("incorrect");
+    }
+
+    optionBtns.forEach((button) => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+
+    setTimeout(() => {
+        optionBtns.forEach((button) => {
+            button.disabled = false;
+            button.classList.remove("correct", "incorrect");
+        });
+        generateNewQuestion();
+    }, 2000);
+}
+
+function nextQuestion(){
+    secondsDisplay.textContent = 15;
+    resetTimer();
+    startTimer();
+    generateNewQuestion();
+    resetOptionBtns();
+    console.log('next btn')
+    // counter++;
+}
+
+function resetOptionBtns() {
+    optionBtns.forEach(button => {
+        button.classList.remove('correct', 'incorrect');
+        button.classList.add('hover');
+        button.removeAttribute('disabled');
+    });
+}
+
+function restartGame(){
+    gameProgress = 0;
+    beginQuiz();
+    resetOptionBtns();
+    clearInterval(secondsDisplay);
+    console.log('restartquiz')
+}
+
+function disableAnswerBtn() {
+    optionBtns.forEach((btn) =>{
+        btn.disabled=true;
+    });
+}
+
+function startTimer(){
+    timeRemaining = 5;
+    timerInterval = setInterval(function (){
+        countdown();
+        secondsDisplay.textContent = timeRemaining;
+    },1000);
+    console.log('start timer')
+}
+
+function countdown(){
+    if (timeRemaining === 0) {
+   disableAnswerBtn();
+    } else {
+        timeRemaining--;
+    }
+    
+    console.log('countdown')
+}
+
+function resetTimer(){
+    clearInterval(time);
+}
+//function incrementScore()
